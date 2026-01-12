671df38bd1c52147054e3ac0bd5ad433e7cb7b4d
Umang Giri <umanggiri103@gmail.com>
2026-01-12 18:26:34 +0545

    dates and price section added

--- js/trips-types.js
+++ js/trips-types.js
@@ -1,16 +1,18 @@
 //Gallery modal
 document.addEventListener("DOMContentLoaded", () => {
-  const openBtn = document.querySelector(".view-all-btn");
   const modal = document.getElementById("galleryModal");
   const closeBtn = document.getElementById("closeGallery");
   const overlay = modal.querySelector(".gallery-modal-overlay");
+  const openButtons = document.querySelectorAll(".view-all-btn, .open-gallery");
 
-  if (!openBtn || !modal) return;
+  if (!modal || openButtons.length === 0) return;
 
-  openBtn.addEventListener("click", (e) => {
-    e.preventDefault();
-    modal.classList.add("active");
-    document.body.style.overflow = "hidden";
+  openButtons.forEach((btn) => {
+    btn.addEventListener("click", (e) => {
+      e.preventDefault();
+      modal.classList.add("active");
+      document.body.style.overflow = "hidden";
+    });
   });
 
   function closeModal() {
@@ -27,5 +29,36 @@ document.querySelectorAll(".save-btn").forEach((btn) => {
   btn.addEventListener("click", (e) => {
     e.stopPropagation();
     btn.classList.toggle("active");
+  });
+});
+
+//itnerary cards click
+document.getElementById("expandAll").addEventListener("click", function () {
+  const items = document.querySelectorAll(".accordion-collapse");
+
+  const expanded = this.classList.toggle("open");
+
+  items.forEach((item) => {
+    const collapse = new bootstrap.Collapse(item, { toggle: false });
+    expanded ? collapse.show() : collapse.hide();
+  });
+
+  this.textContent = expanded ? "Collapse All –" : "Expand All +";
+});
+
+//Date & Prices Section - Toggle
+document.querySelectorAll(".month-btn").forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const month = btn.dataset.month;
+
+    document
+      .querySelectorAll(".month-btn")
+      .forEach((b) => b.classList.remove("active"));
+    btn.classList.add("active");
+
+    document
+      .querySelectorAll(".month-content")
+      .forEach((content) => content.classList.remove("active"));
+    document.getElementById(month).classList.add("active");
   });
 });
